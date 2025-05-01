e0f869aeb8027c8ed3418dcf48c0653f25969ab9
Annabel Nakimuli <nakimuliannabel10@gmail.com>
2025-05-01 13:59:18 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ledgerApp{
+
+    public static void homeScreen(){
+        System.out.println("""
+           Welcome to the home screen
+           Enter an option to proceed:
+           D - Add Deposit
+           P - Make Payment
+           L - Ledger
+           X - Exit
+            """);
+    }
+
+    public static void main(String[] args) {
+        {
+
+        }
+
+
+        }
+
+    }
